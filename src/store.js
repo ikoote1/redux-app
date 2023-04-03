f2dd0514750b8fdb89050e1211afa44ca8283dc6
ikoote1 <ikootepreim@gmail.com>
2023-04-03 16:46:04 +0300

    updating and coding using  redux

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import '.'
+import cartReducer from'./features/card/cardSlice'
 
 export const store = configureStore({
     reducer:{
-
+        cart: cartReducer,
     },
 });
